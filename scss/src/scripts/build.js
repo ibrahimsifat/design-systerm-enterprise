eb9cc7d4693c80e11a53a2036d5d8ae9e034b689
ibrahimsifat <ibsifat24@gmail.com>
2023-03-21 09:51:12 +0300

    compiler-scss-to-css

--- scss/src/scripts/build.js
+++ scss/src/scripts/build.js
@@ -3,6 +3,12 @@ const path = require("path");
 const sass = require("sass");
 
 // compile scss to css function
+/**
+ * compile scss to css
+ * @param {string} src - source file
+ * @param {string} output - output file
+ *
+ */
 const compile = (src, output) => {
   const result = sass.compile(path.resolve(src), {
     style: "expanded",
@@ -11,4 +17,28 @@ const compile = (src, output) => {
   fs.writeFileSync(path.resolve(output), result.css);
 };
 
+// compile global.scss to global.css
 compile("./src/global.scss", "./lib/global.css");
+
+/**
+ * get all components from src folder
+ * @returns {Array} - array of objects with src and output properties
+ */
+const getAllComponents = () => {
+  let allComponents = [];
+  const types = ["atoms", "molecules", "organisms", "templates", "pages"];
+  types.forEach((type) => {
+    const allFiles = fs.readdirSync(`src/${type}`).map((file) => ({
+      src: `src/${type}/${file}`,
+      output: `lib/${file.slice(0, -5)}.css`,
+    }));
+    allComponents = [...allComponents, ...allFiles];
+  });
+  return allComponents;
+};
+// console.log(getAllComponents());
+
+// compile all components
+getAllComponents().forEach((component) => {
+  compile(component.src, component.output);
+});
